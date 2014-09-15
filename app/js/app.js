ad1f83bd8d62d821ddb84b95efad79940b01ccd6
Andrew Huffman <mahuffma@us.ibm.com>
2014-09-15 15:54:26 -0400

    jQuery slider works

--- app/js/app.js
+++ app/js/app.js
@@ -18,14 +18,14 @@ findingHope.controller('MainPageCtrl', function($scope){
     $scope.carousel = {
         slides:[{
             image: '/images/kids_running_bw.jpg',
-            style: 'background-position: 50% 40%;',
+            style: {'background-position':'50% 40%'},
             caption: {
                 quote:"Some people care too much. I think it's called love",
                 author:"A.A. Milne"
             }
         },{
             image: '/images/kids_beach_bw.jpg',
-            style: 'background-position: 50% 60%;',
+            style: {'background-position':'50% 60%'},
             caption: {
                 quote:"Be joyful always; pray continually; give thanks in all circumstances...",
                 author:"1 Thessalonians 5:16-18"
@@ -38,9 +38,6 @@ findingHope.controller('MainPageCtrl', function($scope){
             } 
         }]
     };
-    angular.element('.carousel').carousel({
-        interval: 5000
-    });
     //workaround issue where video element becomes half the size when returning
     //to main page from another page
     var video = angular.element('video')
@@ -50,10 +47,55 @@ findingHope.controller('MainPageCtrl', function($scope){
     console.log("MP $scope: %o", $scope);
 });
 
-findingHope.controller('CarouselController', function($scope){
-    $scope.slides = $scope.$parent.carousel.slides;
-    $scope.init = function() {
-        console.log("images: "+angular.element('.carousel-image').length);
-        console.log("I was called") 
+findingHope.controller('CarouselController', function($scope) {
+    var $ = angular.element;
+    function __createCarouselSlide(slideInfo) {
+        var image = {src: slideInfo.image, style: slideInfo.style};
+        var caption = slideInfo.caption || false;
+        var itemDiv = $('<div></div>').addClass('item');
+        var imageDiv = 
+            $('<div></div>').addClass('carousel-image')
+                .css("background-image", 'url(\''+image.src+'\')');
+        if (image.style) { 
+            Object.keys(image.style).forEach(function(key){
+                imageDiv.css(key, image.style[key]);
+            });
+        }
+        
+        itemDiv.append(imageDiv)
+
+        if (caption) {
+            var quoteContainer = $('<div></div>').addClass('container')
+                .append($('<div></div>').addClass('carousel-caption')
+                    .append($('<h3></h3>').addClass('quote').html('&ldquo;'+caption.quote+'&rdquo;'))
+                    .append($('<h3></h3>').addClass('author').text('- '+caption.author)))
+            itemDiv.append(quoteContainer);
+        }
+
+        return itemDiv
     }
+    var slides = $scope.$parent.carousel.slides || [];
+    var carouselContainer = $('.carousel-inner');
+    slides.forEach(function(slide, idx) {
+        var slideDiv = __createCarouselSlide(slide)
+        if (idx == 0) {
+            slideDiv.addClass('active')
+        }
+        carouselContainer.append(slideDiv)
+    });
+    $('.carousel').carousel({
+        interval: 5000
+    });
 });
+
+
+
+
+
+
+
+
+
+
+
+
